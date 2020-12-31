e81773f886e2a299089ab8e09a9c52f64c76a7ae
anuragchris <55807600+anuragchris@users.noreply.github.com>
2020-12-31 15:21:52 +0530

    author name

--- src/main/java/com/shareme/fileShare/model/FileDetails.java
+++ src/main/java/com/shareme/fileShare/model/FileDetails.java
@@ -1,26 +1,29 @@
 package com.shareme.fileShare.model;
 
+/**
+ * @author Anurag PC
+ *
+ */
 public class FileDetails {
-    
+
     private String fileName;
-    
+
     private byte[] data;
 
     public String getFileName() {
-        return fileName;
+	return fileName;
     }
 
     public void setFileName(String fileName) {
-        this.fileName = fileName;
+	this.fileName = fileName;
     }
 
     public byte[] getData() {
-        return data;
+	return data;
     }
 
     public void setData(byte[] data) {
-        this.data = data;
+	this.data = data;
     }
-   
 
 }
